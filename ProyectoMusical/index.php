6b9576386d4254d53f29adee62f8cd3755ba3a1a
Sergio Enrique Guerra Cervantes <a1252819@uabc.edu.mx>
2020-11-27 14:47:01 -0800

    Avance1.2
    Ya se arreglaron las secciones de inicio y se agrego un logo

--- ProyectoMusical/index.php
+++ ProyectoMusical/index.php
@@ -1,5 +1,5 @@
 <?php
-	include "navegar.php";
+	include "navegacion.php";
 ?>
 <!DOCTYPE html>
 <!--
@@ -13,10 +13,10 @@ Licence URI: https://www.os-templates.com/template-terms
 <html lang="">
 <!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
 <head>
-<title>PHP-PL/SQL</title>
+<title>Base de Datos Musical</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<link href="layout/styles/design.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
 
@@ -25,49 +25,79 @@ Licence URI: https://www.os-templates.com/template-terms
 <div class="wrapper row0">
   <div id="topbar" class="hoc clear"> 
     <div class="fl_left"> 
-      <!-- ################################################################################################ -->
       <ul class="nospace"> 
 		<li><a href="http://www.uabc.mx"                      target="blank"                 title="Universidad Autónoma de Baja California"   ><i class="fas fa-university"> </i></a></li>	  
 		<li><a href="http://fcqi.tij.uabc.mx"                 target="blank"                  title="Facultad de Ciencias Químicas e Ingeniería"><i class="fas fa-university">     </i></a></li>
 		<li><a href="https://cucapa.tij.uabc.mx/computacion/" target="blank" title="Ingeniería en Computación"><i class="fas fa-university"></i></a></li>
     </ul>
-      <!-- ################################################################################################ -->
     </div>
+	
     <div class="fl_right"> 
-      <!-- ################################################################################################ -->
       <ul class="nospace">  
 	  <li><a href="ver_codigo.php?archivo=<?php echo $archivo?>" target="blank" title="Ver código PHP de la opción seleccionada"><i class="fas fa-file-alt"></i></a></li>
       <li><a href="ver_codigo.php?archivo=index.php" target="blank" title="Ver código PHP de index"><i class="fas fa-bars"></i></a></li>
       <li><a href="ver_codigo.php?archivo=navegar.php" target="blank" title="Ver código PHP de estructura de navegación (switch-case)"><i class="fas fa-ship"></i></a></li>
 	  <li><a href="ejemplo_clase_bd.zip" target="blank" title="Descargar scripts PL/SQL de la BD ventas"><i class="fas fa-database"></i></a></li>
     </ul>
-      <!-- ################################################################################################ -->
     </div>
   </div>
 </div>
 
 <div class="wrapper row1">
-  <header id="header" class="hoc clear"> 
-    <!-- ################################################################################################ -->
+  <header id="header" class="hoc clear">
     <section>
       <div></div>
       <div>
        <h3 class="heading">Bases de Datos<br>PROYECTO MUSICAL<br><a href="index.php">Sony Music</a></h3>
       </div>
        
-	   <div><img src="images/fondoMusica.jpg"></div>
+	   <div><img src="images/LogoBaseMusical.png"></div>
     </section>
-    <!-- ################################################################################################ -->
   </header>
 </div>
-<!-- ################################################################################################ -->
 
 <div class="wrapper row2">
-  <nav id="mainav" class="hoc clear"> 
-    <!-- ################################################################################################ -->
+  <nav id="mainav" class="hoc clear">
     <ul class="clear">
-     <li class="active"><a href="index.php">1] Bienvenida</a></li>
 	
+		<li class="active"><a href="index.php">1] Bienvenida</a></li>
+	
+		<li> <a class = "drop" href = "#">2] Musica</a>
+			<ul>
+				<li> <a href = "index.php?opcion_menu=21">1] Canciones </a></li>
+				<li> <a href = "index.php?opcion_menu=22">2] Albumes </a></li>
+				<li> <a href = "index.php?opcion_menu=23">3] Generos </a></li>
+			</ul>
+		</li>
+		
+		<li> <a class = "drop" href = "#">3] Artistas</a>
+			<ul>
+				<li> <a href = "index.php?opcion_menu=31">1] Musicos </a></li>
+				<li> <a href = "index.php?opcion_menu=32">2] Compositores </a></li>
+				<li> <a href = "index.php?opcion_menu=33">3] Premios </a></li>
+			</ul>
+		</li>
+		
+		<li> <a class = "drop" href = "#">4] Otros</a>
+			<ul>
+				<li> <a href = "index.php?opcion_menu=41">1] Persona</a></li>
+				<li> <a href = "index.php?opcion_menu=42">2] Representante</a></li>
+			</ul>
+		</li>
+		
+		<li> <a class = "drop" href = "#">5] Actualizaciones</a>
+			<ul>
+				<li> <a href = "index.php?opcion_menu=51">1] Canciones </a></li>
+				<li> <a href = "index.php?opcion_menu=52">2] Albumes</a></li>
+				<li> <a href = "index.php?opcion_menu=53">3] Generos</a></li>
+				<li> <a href = "index.php?opcion_menu=54">4] Musicos</a></li>
+				<li> <a href = "index.php?opcion_menu=55">5] Compositores</a></li>
+				<li> <a href = "index.php?opcion_menu=56">6] Premios</a></li>
+				<li> <a href = "index.php?opcion_menu=55">7] Persona</a></li>
+				<li> <a href = "index.php?opcion_menu=56">8] Representante</a></li>
+			</ul>
+		</li>
+	<!---
       <li><a class="drop" href="#">2] Consultas</a>
         <ul>
           <li><a href="index.php?opcion_menu=21">1] Tabla Artista</a></li>
@@ -80,6 +110,7 @@ Licence URI: https://www.os-templates.com/template-terms
           <li><a href="index.php?opcion_menu=28">8] Procedimiento Busca Cliente</a></li>
         </ul>
       </li>
+	
 	  
 	  <li><a class="drop" href="#">3] Actualizaciones</a>
         <ul>
@@ -89,26 +120,14 @@ Licence URI: https://www.os-templates.com/template-terms
 		  <li><a href="index.php?opcion_menu=34">4] ABC Clientes</a></li>	
 		</ul> 
 	   </li>
-	   
- <!-- Próximamente      
-	  <li><a class="drop" href="#">4] Procesos específicos</a>
-        <ul>
-          <li><a href="index.php?opcion_menu=41">1] Función Ventas</a></li>
-          <li><a href="index.php?opcion_menu=42">2] Procedximamimiento Factura</a></li>
-        </ul>
-      </li>
--->	  
+	   -->
     </ul>
-    <!-- ################################################################################################ -->
   </nav>
 </div>
 
-<!-- ################################################################################################ -->
 
 <div class="wrapper row3">
   <main class="hoc container clear"> 
-    <!-- main body -->
-    <!-- ################################################################################################ -->
     <div class="sectiontitle">
       <h3 class="heading"><?php echo $encabezado;?></h3>
 	  
@@ -125,13 +144,10 @@ Licence URI: https://www.os-templates.com/template-terms
 <div class="wrapper row5">
 
   <div id="copyright" class="hoc clear"> 
-    <!-- ################################################################################################ -->
     <p class="fl_left">Copyright &copy; 2020 - Todos los derechos reservados - <a href="#">http://omd-prof.xyz/</a></p>
     <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
-    <!-- ################################################################################################ -->
   </div>
 </div>
-<!-- ################################################################################################ -->
 <script src="layout/scripts/jquery.min.js"></script>
 <script src="layout/scripts/jquery.backtotop.js"></script>
 <script src="layout/scripts/jquery.mobilemenu.js"></script>
--- ProyectoMusical/navegacion.php
+++ ProyectoMusical/navegacion.php
@@ -12,14 +12,120 @@
 			$encabezado = 'Inicio';
 		break;
 		
-		case '2'
+		//Musica
+		case '21':
+			$tabla='Cancion';
 			$archivo = 'canciones.html';
-			$encabezado = 'Canciones';
+			$encabezado = 'Canciones '.$tabla;
 		break;
-			
-		case '3'
-			$archivo = 'artistas.html';
-			$artistas = 'Artistas';
-		break;		
+		
+		case '22':
+			$tabla='Album';
+			$archivo = 'albumes.html';
+			$encabezado = 'Albumes '.$tabla;
+		break;
+		
+		case '23':
+			$tabla='GeneroMusical';
+			$archivo = 'generos.html';
+			$encabezado = 'Generos Musicales '.$tabla;
+		break;
+		
+		//Artistas
+		case '31':
+			$tabla='Musico';
+			$archivo = 'canciones.html';
+			$encabezado = 'Canciones '.$tabla;
+		break;
+		
+		case '32':
+			$tabla='Compositor';
+			$archivo = 'compositores.html';
+			$encabezado = 'Compositores '.$tabla;
+		break;
+		
+		case '33':
+			$tabla='Premio';
+			$archivo = 'premios.html';
+			$encabezado = 'Premios '.$tabla;
+		break;
+		
+		//Otros
+		case '41':
+			$tabla='Musico';
+			$archivo = 'canciones.html';
+			$encabezado = 'Canciones '.$tabla;
+		break;
+		
+		case '42':
+			$tabla='Compositor';
+			$archivo = 'compositores.html';
+			$encabezado = 'Compositores '.$tabla;
+		break;
+		
+		//Actualizaciones
+		case '51':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Cancion';
+			$llave = 'idCancion';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Canciones '.$tabla;
+		break;
+		
+		case '52':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Album';
+			$llave = 'Nombre';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Albumes '.$tabla;
+		break;
+		
+		case '53':
+			$opcion_menu='formulario_editable';
+			$tabla = 'GeneroMusical';
+			$llave = 'Nombre';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Generos '.$tabla;
+		break;
+		
+		case '54':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Musico';
+			$llave = 'idMusico';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Musicos '.$tabla;
+		break;
+		
+		case '55':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Compositor';
+			$llave = 'idCompositor';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Compositores '.$tabla;
+		break;
+		
+		case '56':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Premio';
+			$llave = 'idPremio';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Premios '.$tabla;
+		break;
+		
+		case '57':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Persona';
+			$llave = 'RFC';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Personas '.$tabla;
+		break;
+		
+		case '58':
+			$opcion_menu='formulario_editable';
+			$tabla = 'Representante';
+			$llave = 'idRepresentante';
+			$archivo = 'consulta_tabla_musica.php';
+			$encabezado = 'Modificar Representantes '.$tabla;
+		break;
 	}
 ?>
