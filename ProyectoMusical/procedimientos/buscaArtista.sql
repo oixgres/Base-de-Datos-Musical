60cdc58a78215d7f73bc995f4660e4723a6357a5
Miriam Fernanda Cruz Sanchez <54223323+miriamf16@users.noreply.github.com>
2021-01-03 15:01:53 -0800

    Add files via upload
    complemento de procedimientos

--- ProyectoMusical/procedimientos/buscaArtista.sql
+++ ProyectoMusical/procedimientos/buscaArtista.sql
@@ -3,5 +3,8 @@ DROP PROCEDURE IF EXISTS buscaArtista;
 DELIMITER !!
 CREATE PROCEDURE buscaArtista(subcadena VARCHAR(45))
 BEGIN
-	SELECT * FROM Persona WHERE Nombre=subcadena;
+	SELECT Musico.idMusico,Persona.Nombre ,Musico.Descripcion,Musico.Grupo,Premios.Nombre as NombrePremiacion FROM Musico 
+    INNER JOIN Persona ON Musico.RFC = Persona.RFC
+    INNER JOIN Premios ON Musico.Premios_idPremio = Premios.idPremio
+    WHERE Persona.Nombre=subcadena;
 END!!
--- ProyectoMusical/procedimientos/buscaCancion.sql
+++ ProyectoMusical/procedimientos/buscaCancion.sql
@@ -4,6 +4,10 @@ DROP PROCEDURE IF EXISTS buscaCancion;
 DELIMITER !!
 CREATE PROCEDURE buscaCancion(subcadena VARCHAR(45))
 BEGIN
-	SELECT * FROM Cancion WHERE Nombre=subcadena;
+	SELECT Cancion.idCancion,Cancion.Nombre as NombreCancion,Cancion.FechaLanzamiento,Album.Nombre as NombreAlbum,GeneroMusical.Nombre as NombreGenero FROM Cancion 
+	INNER JOIN Album ON Cancion.idAlbum = Album.idAlbum
+	INNER JOIN GeneroMusical ON Cancion.idGenero = GeneroMusical.idGenero
+	WHERE Cancion.Nombre = subcadena;
+
 END!!
 
